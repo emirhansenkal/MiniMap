44b1c615cff2a31ea538602a126f74a1ddc0091f
Emirhan Şenkal <emirhansenkal1@gmail.com>
2021-01-31 16:24:03 +0300

    Initial Project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float _movementSpeed = 10;
+    private float _rotationalSpeed = 100;
+    void FixedUpdate()
+    {
+        transform.Translate(Vector3.forward * (Time.deltaTime * _movementSpeed * Input.GetAxis("Vertical")));
+        transform.Rotate(Vector3.up * (Time.deltaTime * _rotationalSpeed * Input.GetAxis("Horizontal")));
+    }
+}
